0cb346eb583200e96a2e8faebccd99239b50d2c3
aleksander meleshko <meleshkoad36@gmail.com>
2022-12-11 17:32:43 +0300

    Add dz5.py

--- dz5.py
+++ dz5.py
@@ -4,29 +4,10 @@
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
 
-def inputNumbers(x):
-    xy = ["X", "Y"]
-    a = []
-    for i in range(x):
-        is_OK = False
-        while not is_OK:
-            try:
-                number = int(input(f"Введите координату по {xy[i]}: "))
-                a.append(number)
-                is_OK = True
-            except ValueError:
-                print("Вводить надо целые числа!")
-    return a
+x_a = int(input())
+y_a = int(input())
+x_b = int(input())
+y_b = int(input())
 
-
-def calculateLengthSegment(a, b):
-    lengthSegment = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** (0.5)
-    return lengthSegment
-
-
-print("Введите координаты точки А")
-pointA = inputNumbers(2)
-print("Введите координаты точки В")
-pointB = inputNumbers(2)
-
-print(f"Длина отрезка: {format(calculateLengthSegment(pointA, pointB), '.2f')}")
+res = ((x_a - x_b)**2+(y_a - y_b)**2)**0.5
+print(round(res, 2))
