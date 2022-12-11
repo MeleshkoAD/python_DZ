72e8573cf77e410eb4ac89b1e54e1b3a1a1ef246
aleksander meleshko <meleshkoad36@gmail.com>
2022-12-11 17:31:19 +0300

    Add dz1.py

--- dz1.py
+++ dz1.py
@@ -4,25 +4,10 @@
 # - 6 -> да
 # - 7 -> да
 
-def InputNumbers(inputText):
-    is_OK = False
-    while not is_OK:
-        try:
-            number = int(input(f"{inputText}"))
-            is_OK = True
-        except ValueError:
-            print("Это не число!")
-    return number
-
-
-def checkNumber(num):
-    if 6 <= num <= 7:
-        print("Yes")
-    elif 0 < num < 6:
-        print("No")
-    else:
-        print("число вне пределов 7 дней")
-
-
-num = InputNumbers("Введите число: ")
-checkNumber(num)
+a = int(input())
+if a >= 6 and a <= 7:
+    print('Выходной день')
+elif a > 0 and a <= 5:
+    print('Будний день')
+else:
+    print('Error')
